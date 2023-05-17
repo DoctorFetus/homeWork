e16c7cfd9071eb152b7fc59f3359df566af48a1a
Mikhail Palkin <thefireofcoall@gmail.com>
2023-05-17 16:38:13 +0300

    hw8 table

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            const sortByName = (a: UserType, b: UserType) => {
+                return (a.name > b.name) ? 1 : -1
+            }
+            const sortedUsers = state.map(u => ({...u})).sort(sortByName)
+
+            return action.payload === "up"
+                ? sortedUsers
+                : sortedUsers.reverse()// need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= 18) // need to fix
         }
         default:
             return state
