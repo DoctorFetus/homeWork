94bdce0782b4df4061ccbe20d3a94a9721653744
Mikhail Palkin <thefireofcoall@gmail.com>
2023-05-27 23:12:47 +0300

    hw10 loading

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -5,6 +5,7 @@ import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
+import style from './Loader.module.css'
 
 /*
 * 1 - в файле loadingReducer.ts дописать типы и логику
@@ -14,20 +15,20 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
+    const dispatch = useDispatch()
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType>(store => store.loading.isLoading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
-        // setTimeout
+        dispatch(loadingAC(true))
+        setTimeout(() => dispatch(loadingAC(false)), 1500)
     }
-
+    console.log(isLoading)
     return (
         <div id={'hw10'}>
             <div className={s2.hwTitle}>Homework #10</div>
 
-            <div className={s2.hw}>
+            <div className={`${s2.hw} ${style.container}`}>
                 {isLoading ? (
                     <div id={'hw10-loading'}>
                         <Loader/>
